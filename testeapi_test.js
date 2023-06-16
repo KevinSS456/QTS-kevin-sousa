04555d45b67deb66f4eb777cb65ecfdd5c29660b
kesantiago456@gmail.com <kesantiago456@gmail.com>
2023-06-16 08:36:17 -0300

    Criação de Asserts

--- testeapi_test.js
+++ testeapi_test.js
@@ -2,12 +2,23 @@ import{
     assertEquals,
     assertStringIncludes,
     assertArrayIncludes,
-} from "http:"
+} from "http://deno.land/std/testing/asserts.ts";
 
 let aluno = 'Crovis Jose'
 let professores = ['Jose','Lais','Davi','Bruno Aparecido Cano']
+let numeros = [11, 24,69]
 
 Deno.test("Teste de assertions (API)", () => {
     assertEquals(aluno, "Crovis Jose");
-    assertStringIncludes(aluno, "Jose")
+    assertStringIncludes(aluno, "Jose");
+})
+
+Deno.test("Teste de assertivos (API)", () =>{
+    assertArrayIncludes(professores
+        ["Davi","Lais","Alan"],
+    "Opa! Algo deu errado");
+
+})
+Deno.test("Testando numeros", () =>{
+       assertArrayIncludes(numeros,[11])
 })
